c53a205723d05173f56b5b46bc8674af5b65574e
Pradeep Landge <pradeeplandge1@gmail.com>
2023-04-25 21:21:12 +0530

    Update login_to_org.sh

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -3,4 +3,4 @@ mkdir keys
 echo $CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant -i $APP_KEY -f keys/server.key --username $SF_USERNAME -d -a DevHub
