55fc6ef1c7ff7cdd7ecbb367118d863b3026c1b2
Pradeep Landge <pradeeplandge1@gmail.com>
2023-04-27 09:41:22 +0530

    Update run_apex_tests.sh

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -1,21 +1,17 @@
-# Authenticate to salesforce
-#echo "Authenticating..."
-#sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+set -eo pipefail
 
-#Create a scratch org
-#echo "Creating the Scratch Org..."
-#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+echo "Authenticating with Salesforce CLI"
+echo $SALESFORCE_JWT_KEY > jwt.key
+sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SALESFORCE_USERNAME --setdefaultdevhubusername
 
-#sfdx force:source:push -u ${CIRCLE_BRANCH}
+echo "Creating scratch org"
+sfdx force:org:create -f config/project-scratch-def.json -a scratch-org -s
 
-#echo "Coverting source to metadata format"
-#sfdx force:source:convert -d test_code -r force-app
+echo "Deploying metadata to scratch org"
+sfdx force:source:deploy -p force-app/main/default -u scratch-org
 
-#echo "Deploying code to org"
-#sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
+echo "Running Apex tests"
+sfdx force:apex:test:run -u scratch-org -w 5
 
-#echo "Deploying source to org"
-#sfdx force:source:deploy --sourcepath force-app --target-org DevHub
-
-     sfdx force:source:convert -r force-app -d testDeploy
-     sfdx force:mdapi:deploy -d testDeploy/ --target-org deploy -w 2
+echo "Deleting scratch org"
+sfdx force:org:delete -p -u scratch-org
